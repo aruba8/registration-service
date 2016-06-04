e8527b5db48a7a291cbdd8155a349abafeb37da3
MaksimP <maksimp86@mail.ru>
2016-06-05 00:15:49 +0300

    move

---
+++ src/main/java/com/jetminds/service/SendEmailService.java
@@ -0,0 +1,17 @@
+package com.jetminds.service;
+
+import java.util.Map;
+
+/**
+ * Service snd email.
+ */
+public interface SendEmailService {
+
+    /**
+     * Send email.
+     * @param receiveMessage message received.
+     * @return flag for test.
+     *
+     */
+    boolean sendEmail(Map<String, String> receiveMessage);
+}
