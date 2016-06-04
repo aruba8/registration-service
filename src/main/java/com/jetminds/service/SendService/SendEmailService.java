76ed06d3b1c568f068e425f00c2994a10b4825a4
MaksimP <maksimp86@mail.ru>
2016-06-04 23:27:02 +0300

    fix checkstyle error

--- src/main/java/com/jetminds/service/SendService/SendEmailService.java
+++ src/main/java/com/jetminds/service/SendService/SendEmailService.java
@@ -2,7 +2,16 @@ package com.jetminds.service.SendService;
 
 import java.util.Map;
 
+/**
+ * Service snd email.
+ */
 public interface SendEmailService {
 
-    void receiveMessage(Map<String, String> receiveMessage);
+    /**
+     * Send email.
+     * @param receiveMessage message received.
+     * @return flag for test.
+     *
+     */
+    boolean sendEmail(Map<String, String> receiveMessage);
 }
