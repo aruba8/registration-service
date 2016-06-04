29aba067a02e77afe4efe1b8a7d34b7f74ceb6b4
MaksimP <maksimp86@mail.ru>
2016-06-04 22:21:51 +0300

    fix checkstyle error

--- src/main/java/com/jetminds/config/QueueConfig.java
+++ src/main/java/com/jetminds/config/QueueConfig.java
@@ -6,11 +6,21 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.jms.Queue;
 
+/**
+ * Class configuration queue.
+ */
 @Configuration
 public class QueueConfig {
 
+    /**
+     * Name queue.
+     */
     public static final String NAME_QUEUE = "message_queue";
 
+    /**
+     * Bean queue.
+     * @return Queue.
+     */
     @Bean
     public Queue getQueueBroker() {
         return new ActiveMQQueue(NAME_QUEUE);
